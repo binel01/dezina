8b7aa0229fd1431682aeaec55757bca96e6d197c
Hervé Arsène BINELI MANGA <herve-arsene.bineli-manga@bureauveritas.com>
2018-10-25 16:56:32 +0100

    Ajout du slider à la page Best Seller

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ProductServiceProvider } from '../../providers/product-service/product-service';
+
+
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  public bestSellerProducts = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private productService: ProductServiceProvider) {
+  	console.log("Best Seller page constructed !");
+  }
 
+  ionViewDidLoad() {
+  	console.log("About Page loaded !!")
+  	this.productService.getProducts()
+  		.subscribe((allProducts) => {
+  			this.bestSellerProducts = allProducts.filter(product => product.bestSeller == true);
+  			console.log(this.bestSellerProducts);
+  		});
   }
 
 }
